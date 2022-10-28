530f6f9e61bf79d9992eafae9f4390cd568e1e2d
Wilson Muniz Fonseca <wilsonwmfsl@gmail.com>
2022-10-28 01:01:02 -0300

    aprendizado de máquina

---
+++ BootCamp_UNIMED_BH/Projeto_Criação_Pacotes/package-template-master/README.md
@@ -0,0 +1,31 @@
+# 					Photo_Album_WilsonMuniz
+
+#### 
+
+### Descricao 
+
+#### O pacote Photo_Album e usado para:
+
+######  e usado para montar um album de fotos com base nas imagens disponiveis em um diretorio de escolha do usuario. As imagens sao convertidas para o padrao "pdf", salvas em um arquivo em um diretorio de escolha do usuario.
+
+
+
+### Instalacao
+
+###### Use o gerenciador de pacotes "pip" para instalar Photo_Album_WilsonMuniz.
+
+​						**pip install Photo_Album_WilsonMuniz**
+
+ **Usage**
+
+###### **from** Photo_Album_WilsonMuniz**.**assembly_album **import** assembly_photo_album
+
+###### assembly_photo_album()
+
+### Author
+
+###### Wilson Muniz Fonseca
+
+### Licenca
+
+###### **WMF**
---
+++ BootCamp_UNIMED_BH/Projeto_Criação_Pacotes/package-template-master/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+    
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    
+    name="Photo_Album_WilsonMuniz",
+    version="0.0.1",
+    author="Wilson Muniz Fonseca",
+    author_email="pc-wilson@hotmail.com",
+    description="O pacote Photo_Album e usado para montar um album de fotos baseado em imagens que são convertidas para o padrão 'pdf'.",
+    long_description=page_description,   
+    long_description_content_type="text/markdown",
+    url="https://github.com/WisonMuniz/BootCamp_UNIMED_BH/Projeto_Criacao_Pacotes",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)    
